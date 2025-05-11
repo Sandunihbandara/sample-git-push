85bd049e9527083d5fc08c5b096fb3d97e1aceaf
Sandunihbandara <sandunihbandara252@gmail.com>
2025-05-11 17:28:48 +0530

    My First Commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test Git</title>
+</head>
+<body>
+    <h1>Test git</h1>
+    <p>This is my first commit</p>
+
+    <h1>trrdstdtsf</h1>
+    <p>Now push</p>
+</body>
+</html>
---
+++ js/main.js
@@ -0,0 +1,5 @@
+function abc() {
+  var x = 10;
+
+  var y = 1.5;
+}
